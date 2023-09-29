26bedd78ad64e29983f7a0d92e00e226e770ae75
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-09-29 13:36:52 +0530

    Login page is done 🗝️🛡️

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username = '';
+  password= '';
+  errorMessage = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  login() {
+    if (this.username.trim().length === 0) {
+      this.errorMessage = "Username is required";
+    } else if (this.password.trim().length === 0) {
+      this.errorMessage = "Password is required";
+    } else {
+      this.errorMessage = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home'])
+      } else if (res === 403) {
+        this.errorMessage = "Invalid Credentials!"
+      }
+    }
+  }
 }
