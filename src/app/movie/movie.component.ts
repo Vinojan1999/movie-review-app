317010286fbfd706972953fa2ea88c322b18b268
Vinojan Veerapathrathasan <48822560+Vinojan1999@users.noreply.github.com>
2023-09-30 02:36:51 +0530

    Update movie.component.ts

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -22,13 +22,13 @@ export class MovieComponent {
     this.id = this.route.snapshot.params['id'];
 
     if (this.type === 'trending') {
-      this.url = 'http://localhost:4200/assets/data/trending-movies.json'
+      this.url = 'https://movie-review-app-lac.vercel.app/assets/data/trending-movies.json'
     }
     if (this.type === 'theatre') {
-      this.url = 'http://localhost:4200/assets/data/theatre-movies.json'
+      this.url = 'https://movie-review-app-lac.vercel.app/assets/data/theatre-movies.json'
     }
     if (this.type === 'popular') {
-      this.url = 'http://localhost:4200/assets/data/popular-movies.json'
+      this.url = 'https://movie-review-app-lac.vercel.app/assets/data/popular-movies.json'
     }
 
     this.getMovie();
